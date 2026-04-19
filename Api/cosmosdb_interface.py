ff1f297e291f761e71de99aceaa0b087c17c2d7a
lab-innovacion-caf <innovacion@caf.com>
2026-04-19 19:00:44 -0400

    Delete Api directory

--- Api/cosmosdb_interface.py
+++
@@ -1,2 +0,0 @@
-APP_NAME =  "azfun-POC-VINCULADOR-CR"
-API_URL_BASE = "https://azfun-poc-vinculador-cr.azurewebsites.net/api"
--- Api/recognizer_repository.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class RecognizerInterface(ABC):
-    @abstractmethod
-    def begin_analyze_document(self,file_stream, file_name):
-        pass
